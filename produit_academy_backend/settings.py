fe500ef43b1269ee22aca5ad438f805af042f0f1
ras-al <rasalkpk66@gmail.com>
2026-04-04 19:29:31 +0530

    cros probelm

--- produit_academy_backend/settings.py
+++ produit_academy_backend/settings.py
@@ -86,6 +86,7 @@ else:
     CORS_ALLOWED_ORIGINS = cors_env.split(',')
     CSRF_TRUSTED_ORIGINS = cors_env.split(',')
 
+CORS_ALLOW_CREDENTIALS = True
 # --- REST FRAMEWORK ---
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
